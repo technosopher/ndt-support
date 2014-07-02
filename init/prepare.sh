a75ed299286f168b955d2fc4ea374bc4ddfd743e
Jordan McCarthy <mccarthy@opentechinstitute.org>
2014-07-02 14:33:12 -0400

    Updated all external package pointers to current versions

--- init/prepare.sh
+++ init/prepare.sh
@@ -36,7 +36,7 @@ pushd $SOURCE_DIR/web100_userland-1.8
 popd
 
 # NOTE: unpacked from tar-archives by bootstrap.sh
-pushd $SOURCE_DIR/ndt-3.6.4
+pushd $SOURCE_DIR/ndt-read-only
     export CPPFLAGS="-I$BUILD_DIR/build/include -I$BUILD_DIR/build/include/web100"
     export LDFLAGS="-L$BUILD_DIR/build/lib"
     patch -p0 < $SOURCE_DIR/web100-pcap.c.diff 
